8d09fdaa9fb9cc3ab44732e6887f35f956b036b3
Kevin Souza <kevinwillyansouza@gmail.com>
2023-03-01 17:27:19 -0400

    Adicionando uma parte de JavaScript no controls

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -5,47 +5,113 @@ const audioControllerPrev = document.querySelector('#audio-prev');
 const audioControllerPlay = document.querySelector('#audio-play');
 const audioControllerNext = document.querySelector('#audio-next');
 
+const coverCurrentMusic = document.querySelector('.cover-current-music img');
+const titleCurrentMusic = document.querySelector('.title-info-current-music');
+const genderCurrentMusic = document.querySelector('.gender-info-current-music');
+
+const musicAnimationStatus = document.querySelector('.music-animation-status');
+
 let musicData = [
-    'https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20Numb%20(Lyrics).mp3',
-    'https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20I%20Wanna%20Be%20Your%20Slave%20(Lyrics).mp3',
-    'https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20Last%20Friday%20Night%20(T.G.I.F.)%20(Rock%20Version)%20(Lyrics).mp3',
-    'https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20Bad%20Romance%20(Rock%20Version)%20(Lyrics).mp3',
-    'https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20Without%20Me%20(Rock%20Version)%20(Lyrics).mp3'
+    {
+        id: 1,
+        audioUrl: "https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20Numb%20(Lyrics).mp3",
+        coverUrl: "https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20Numb%20(Lyrics).jpg",
+        title: "Nightcore - Numb (Lyrics)",
+        gender: "Nightcore",
+        theme: "Original"
+    },
+    {
+        id: 2,
+        audioUrl: "https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20I%20Wanna%20Be%20Your%20Slave%20(Lyrics).mp3",
+        coverUrl: "https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20I%20Wanna%20Be%20Your%20Slave%20(Lyrics).jpg",
+        title: "Nightcore - I Wanna Be Your Slave (Lyrics)",
+        gender: "Nightcore",
+        theme: "Original"
+    },
+    {
+        id: 3,
+        audioUrl: "https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20Last%20Friday%20Night%20(T.G.I.F.)%20(Rock%20Version)%20(Lyrics).mp3",
+        coverUrl: "https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20Last%20Friday%20Night%20(Rock%20Version)%20(Lyrics).jpg",
+        title: "Nightcore - Last Friday Night (T.G.I.F.) (Rock Version) (Lyrics)",
+        gender: "Nightcore",
+        theme: "Rock Version"
+    },
+    {
+        id: 4,
+        audioUrl: "https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20Bad%20Romance%20(Rock%20Version)%20(Lyrics).mp3",
+        coverUrl: "https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20Bad%20Romance%20(Rock%20Version)%20(Lyrics).jpg",
+        title: "Nightcore - Bad Romance (Rock Version) (Lyrics)",
+        gender: "Nightcore",
+        theme: "Rock Version"
+    },
+    {
+        id: 5,
+        audioUrl: "https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20Without%20Me%20(Rock%20Version)%20(Lyrics).mp3",
+        coverUrl: "https://pw-music-database.kevinsouza456.repl.co/Nightcore%20-%20Without%20Me%20(Rock%20Version)%20(Lyrics).jpg",
+        title: "Nightcore - Without Me (Rock Version) (Lyrics)",
+        gender: "Nightcore",
+        theme: "Rock Version"
+    }
 ]
 
 let audioControllerPlayToggle = true;
 audioControllerPlay.addEventListener("click", audioControllerPlayFunction)
 audioControllerNext.addEventListener("click", audioControllerNextFunction)
+audioControllerPrev.addEventListener("click", audioControllerPrevFunction)
 
 function inicia(){
-    audioGlobal.src = musicData[0];
+    audioGlobal.src = musicData[0].audioUrl;
+    coverCurrentMusic.src = musicData[0].coverUrl;
+    titleCurrentMusic.innerHTML = musicData[0].title;
+    genderCurrentMusic.innerHTML = musicData[0].gender;
 }
 
 function audioControllerPlayFunction(){
     if(audioControllerPlayToggle){
         audioGlobal.play();
         audioControllerPlayToggle = false;
+        audioControllerPlay.name = 'pause-circle';
+        musicAnimationStatus.classList.add('run');
     }
     else {
         audioGlobal.pause();
         audioControllerPlayToggle = true;
+        audioControllerPlay.name = 'play-circle';
+        musicAnimationStatus.classList.remove('run');
     }
+}
+function audioControllerPlayFunctionNoPause(){
+    audioGlobal.play();
+    audioControllerPlayToggle = false;
+    audioControllerPlay.name = 'pause-circle';
+    musicAnimationStatus.classList.add('run');
 }
 
 function audioControllerNextFunction(){
-    let next;
-    next = indexAudio == 0 ? next = 0 : next = indexAudio + 1
-    console.log(indexAudio)
-
     indexAudio++;
-    if(indexAudio > musicData.length - 1){
+    if(indexAudio >= musicData.length){
         indexAudio = 0;
     }
 
-    audioGlobal.src = musicData[next];
-    audioControllerPlayToggle = true;
+    audioGlobal.src = musicData[indexAudio].audioUrl;
+    coverCurrentMusic.src = musicData[indexAudio].coverUrl;
+    titleCurrentMusic.innerHTML = musicData[indexAudio].title;
+    genderCurrentMusic.innerHTML = musicData[indexAudio].gender;
 
-    audioControllerPlayFunction()
+    audioControllerPlayFunctionNoPause()
+}
+function audioControllerPrevFunction(){
+    indexAudio--;
+    if(indexAudio < 0){
+        indexAudio = musicData.length - 1;
+    }
+
+    audioGlobal.src = musicData[indexAudio].audioUrl;
+    coverCurrentMusic.src = musicData[indexAudio].coverUrl;
+    titleCurrentMusic.innerHTML = musicData[indexAudio].title;
+    genderCurrentMusic.innerHTML = musicData[indexAudio].gender;
+
+    audioControllerPlayFunctionNoPause()
 }
 
 inicia();
