2a9174b1e912f6872cee26e2da265795b2f24a3a
Kevin Souza <kevinwillyansouza@gmail.com>
2023-04-17 10:57:30 -0400

    Adicionado mensagem de termos de uso

--- public/JS/home.js
+++ public/JS/home.js
@@ -1,5 +1,8 @@
 const signup = document.querySelector("#signup");
 const login = document.querySelector("#login");
+const termsOfUse = document.querySelector(".terms-of-use");
+const termsOfUseText = document.querySelector(".terms-of-use-text");
+const termsOfUseClose = document.querySelector("#terms-of-use-close");
 
 signup.addEventListener("click", () => {
     window.location = '/signup'
@@ -9,4 +12,11 @@ login.addEventListener("click", () => {
 })
 document.querySelector('.service-logo').addEventListener("click", () => {
     window.location = '/home'
+})
+
+termsOfUse.addEventListener("click", () => {
+    termsOfUseText.classList.remove("hidden");
+})
+termsOfUseClose.addEventListener("click", () => {
+    termsOfUseText.classList.add("hidden");
 })
