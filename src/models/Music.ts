5d30112d18bd9b2cc89e0e57ebf9de3497f97a23
Kevin Souza <kevinwillyansouza@gmail.com>
2023-11-08 13:19:54 -0400

    fix: adição de restrição única para o campo title do schema music

--- src/models/Music.ts
+++ src/models/Music.ts
@@ -15,10 +15,10 @@ const musicSchema = new Schema<IMusic>({
     _id: { type: String, required: true },
     audioUrl: { type: String, required: true },
     coverUrl: { type: String, required: true },
-    title: { type: String, required: true },
+    title: { type: String, required: true, unique: true },
     gender: { type: String, required: true },
     theme: { type: String, required: true },
-    isVideo: { type: Boolean, default: false},
+    isVideo: { type: Boolean, default: false },
     additionDate: { type: Date, required: true },
 })
 
